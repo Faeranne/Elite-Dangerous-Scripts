f0ac7e9ee6f218156afcf3d4c3859a0b546f0e01
xaduha <xaduha@gmail.com>
2015-01-06 08:13:28 +0300

    Initial

---
+++ Launcherless.ps1
@@ -0,0 +1,17 @@
+﻿$info =  Get-WmiObject Win32_Process -Filter "name = 'EliteDangerous32.exe'"
+
+if ($info) {
+
+    $token = $info.CommandLine -split "  "
+    $desktop = [Environment]::GetFolderPath("Desktop")
+    $WshShell = New-Object -ComObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($desktop + '\Elite Launcherless.lnk')
+    $Shortcut.TargetPath = $info.Path
+    $Shortcut.Arguments = $token[1]
+    $Shortcut.WorkingDirectory = $info.Path | Split-Path
+    $Shortcut.Save()
+
+    'Done!'
+} else {
+    'Error: Launch Elite Dangerous first.'
+}
---
+++ README.txt
@@ -0,0 +1,9 @@
+Launcherless.ps1 - Powershell script that creates a shortcut, allows to skip the launcher and start Elite: Dangerous directly.
+
+Usage:
+
+0) Download and extract the script.
+1) Start Elite: Dangerous using the official launcher. Alt-tab back.
+2) Use right click on Launcherless.ps1 and select 'Run with Powershell'.
+
+A new shortcut will be saved on your Desktop named 'Elite Launcherless' that contains your auth token.
