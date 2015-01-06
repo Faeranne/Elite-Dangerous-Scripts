e0a2c75cf9997ac232a61a5cf83d9feeacee70d0
xaduha <xaduha@gmail.com>
2015-01-06 18:31:34 +0300

    Empty path workaround

--- Launcherless.ps1
+++ Launcherless.ps1
@@ -1,14 +1,16 @@
 ﻿$info =  Get-WmiObject Win32_Process -Filter "name = 'EliteDangerous32.exe'"
 
+$process = Get-Process EliteDangerous32
+
 if ($info) {
 
     $token = $info.CommandLine -split "ServerToken"
     $desktop = [Environment]::GetFolderPath("Desktop")
     $WshShell = New-Object -ComObject WScript.Shell
     $Shortcut = $WshShell.CreateShortcut($desktop + '\Elite Launcherless.lnk')
-    $Shortcut.TargetPath = $info.Path
+    $Shortcut.TargetPath = $process.Path
     $Shortcut.Arguments = '"ServerToken ' + $token[1]
-    $Shortcut.WorkingDirectory = $info.Path | Split-Path
+    $Shortcut.WorkingDirectory = $process.Path | Split-Path
     $Shortcut.Save()
 
     'Done!'
