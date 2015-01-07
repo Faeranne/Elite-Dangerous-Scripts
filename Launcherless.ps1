ea2b77f51316488a7732b256084933861ca0339a
xaduha <xaduha@gmail.com>
2015-01-07 04:55:47 +0300

    Added 'override' comment

--- Launcherless.ps1
+++ Launcherless.ps1
@@ -4,15 +4,19 @@ $info =  Get-WmiObject Win32_Process -Filter "name = 'EliteDangerous32.exe'"
 
 $process = Get-Process EliteDangerous32
 
+$path = $process.Path
+#Override with a verbatim path in quotes if path is empty, like so:
+#$path = 'C:\Program Files (x86)\Frontier\EDLaunch\Products\FORC-FDEV-D-1003\EliteDangerous32.exe'
+
 if ($info) {
 
     $token = $info.CommandLine -split "ServerToken"
     $desktop = [Environment]::GetFolderPath("Desktop")
     $WshShell = New-Object -ComObject WScript.Shell
     $Shortcut = $WshShell.CreateShortcut($desktop + '\Elite Launcherless.lnk')
-    $Shortcut.TargetPath = $process.Path
+    $Shortcut.TargetPath = $path
     $Shortcut.Arguments = '"ServerToken ' + $token[1]
-    $Shortcut.WorkingDirectory = $process.Path | Split-Path
+    $Shortcut.WorkingDirectory = $path | Split-Path
     $Shortcut.Save()
 
     'Done!'
